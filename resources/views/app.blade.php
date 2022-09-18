a61e2ba068209eba10b437322dc8cb39763939f0
Alan <realalansan@gmail.com>
2022-09-18 13:50:28 -0700

    Init base repo

---
+++ resources/views/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+    <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Inter:wght@400;500;600;700&display=swap">
+
+    @routes
+    @viteReactRefresh
+    @vite('resources/ts/app.jsx')
+    @inertiaHead
+</head>
+
+<body class="font-sans subpixel-antialiased">
+    @inertia
+</body>
+
+</html>
