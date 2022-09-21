b5395ac71669b901df1086c1a48059228f2e3836
Alan <realalansan@gmail.com>
2022-09-20 23:21:43 -0700

    Store profile model, controller, migration, request

---
+++ database/migrations/2022_09_19_203654_create_profiles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('profile_picture_url');
+            $table->string('banner_picture_url');
+            $table->string('username');
+            $table->string('biography');
+            $table->string('website');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile');
+    }
+};
