80adb770f3406e1db05b46b4555b3ba7895bd635
Alan <realalansan@gmail.com>
2022-09-23 15:31:26 -0700

    Fix table name

--- database/migrations/2022_09_19_203654_create_profiles_table.php
+++ database/migrations/2022_09_19_203654_create_profiles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('profile', function (Blueprint $table) {
+        Schema::create('profiles', function (Blueprint $table) {
             $table->id();
 
             $table->foreignId('user');
