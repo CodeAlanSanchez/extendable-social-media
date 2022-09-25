c850e55278881b9e6ef1af4d4abbd3305ea10727
Alan <realalansan@gmail.com>
2022-09-24 19:55:51 -0700

    Make children optional

--- resources/ts/Layouts/Basic.tsx
+++ resources/ts/Layouts/Basic.tsx
@@ -2,7 +2,7 @@ import Footer from "@/Components/Footer";
 import Header from "@/Components/Header";
 
 interface Props {
-    children: React.ReactNode;
+    children?: React.ReactNode;
 }
 
 const Basic: React.FC<Props> = ({ children }: Props) => {
