4b8b7807f7cfd60273600fdc0e3b807933e3afca
Alan <realalansan@gmail.com>
2022-09-24 19:55:59 -0700

    Fix key name

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,7 +16,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        return Inertia::render('Posts/Index', ['post' => Post::paginate(18)]);
+        return Inertia::render('Posts/Index', ['posts' => Post::paginate(18)]);
     }
 
     /**
