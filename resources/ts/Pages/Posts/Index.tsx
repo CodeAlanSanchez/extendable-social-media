fc571954d740214a470901fce6749bdf61c7ed8b
Alan <realalansan@gmail.com>
2022-09-26 19:28:31 -0700

    Show list of posts on post page

--- resources/ts/Pages/Posts/Index.tsx
+++ resources/ts/Pages/Posts/Index.tsx
@@ -1,7 +1,24 @@
-interface Props {}
+import PostItem from "@/Components/Post/PostItem";
+import Basic from "@/Layouts/Basic";
+import { Post } from "@/Types/models";
+import { Paginate } from "@/Types/requests";
 
-const index: React.FC<Props> = ({}: Props) => {
-    return <></>;
+interface Props {
+    posts?: Paginate<Post>;
+}
+
+const index: React.FC<Props> = ({ posts }: Props) => {
+    return (
+        <Basic>
+            <div className="p-1 sm:p-8 min-h-screen">
+                <div className="grid grid-flow-row grid-cols-[repeat(auto-fill,minmax(15rem,1fr))] gap-0 md:gap-4">
+                    {posts?.data.map((post) => (
+                        <PostItem key={post.id} post={post} />
+                    ))}
+                </div>
+            </div>
+        </Basic>
+    );
 };
 
 export default index;
