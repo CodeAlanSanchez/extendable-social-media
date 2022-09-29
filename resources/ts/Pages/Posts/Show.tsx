ec95b1f94f9c51d0773b78e7f4123a51d3c71ae7
Alan <realalansan@gmail.com>
2022-09-28 23:22:24 -0700

    Create basic page for individual post

--- resources/ts/Pages/Posts/Show.tsx
+++ resources/ts/Pages/Posts/Show.tsx
@@ -1,4 +1,6 @@
+import UsernameLink from "@/Components/Account/UsernameLink";
 import PostItem from "@/Components/Post/PostItem";
+import Basic from "@/Layouts/Basic";
 import { Post } from "@/Types/models";
 
 interface Props {
@@ -7,9 +9,21 @@ interface Props {
 
 const Show: React.FC<Props> = ({ post }: Props) => {
     return (
-        <div>
-            <PostItem post={post} />
-        </div>
+        <Basic>
+            <div className="max-w-2xl mt-16 h-72 mx-auto flex flex-row p-4 bg-slate-100 rounded shadow">
+                <div className="bg-white mr-8">
+                    <img src={post.image_url} alt={post.caption} />
+                </div>
+                <div className="border-l-2 border-slate-200 pl-8">
+                    <h1>
+                        <UsernameLink username={post.user.username} />
+                        &nbsp;
+                        {post.caption}
+                    </h1>
+                    <p>{post.likes}</p>
+                </div>
+            </div>
+        </Basic>
     );
 };
 
