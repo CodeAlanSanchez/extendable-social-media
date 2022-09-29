dd0c4ca37345aa2978faa59ca98686fbc1510b2f
Alan <realalansan@gmail.com>
2022-09-28 23:22:11 -0700

    Wrap post in anchor tag

--- resources/ts/Components/Post/PostItem.tsx
+++ resources/ts/Components/Post/PostItem.tsx
@@ -15,14 +15,15 @@ const PostItem: React.FC<Props> = ({ post }: Props) => {
     }, [post.caption]);
 
     return (
-        <div
+        <a
             className={`h-80 shadow-lg leading-4 bg-[url('img/${post.image_url})] rounded-[.266rem]`}
+            href={`/posts/${post.id}`}
         >
             <div className="text-white flex flex-col justify-end align-bottom bg-gradient-to-t from-black to-transparent min-w-full p-4 h-full rounded-[.266rem]">
                 <h4 className="font-bold text-lg">{post.user.username}</h4>
                 <p className="leading-5 text-sm">{caption}</p>
             </div>
-        </div>
+        </a>
     );
 };
 
