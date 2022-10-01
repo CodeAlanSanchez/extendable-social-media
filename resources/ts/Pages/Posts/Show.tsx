7a3b8f46d9f3718da8cfa837b41730ce1edfaa90
Alan <realalansan@gmail.com>
2022-09-30 23:28:19 -0700

    Add comment features to post

--- resources/ts/Pages/Posts/Show.tsx
+++ resources/ts/Pages/Posts/Show.tsx
@@ -1,27 +1,34 @@
 import UsernameLink from "@/Components/Account/UsernameLink";
-import PostItem from "@/Components/Post/PostItem";
+import CommentForm from "@/Components/Comment/CommentForm";
+import CommentItem from "@/Components/Comment/CommentItem";
 import Basic from "@/Layouts/Basic";
-import { Post } from "@/Types/models";
+import { Comment, Post, User } from "@/Types/models";
 
 interface Props {
-    post: Post;
+    post: Post & { comments: (Comment & { user: User })[] };
 }
 
 const Show: React.FC<Props> = ({ post }: Props) => {
     return (
         <Basic>
-            <div className="max-w-2xl mt-16 h-72 mx-auto flex flex-row p-4 bg-slate-100 rounded shadow">
-                <div className="bg-white mr-8">
-                    <img src={post.image_url} alt={post.caption} />
+            <div className="max-w-2xl mx-auto mt-16">
+                <div className="flex flex-row bg-slate-100 rounded shadow h-72 p-4 mb-8">
+                    <div className="bg-white mr-8">
+                        <img src={post.image_url} alt={post.caption} />
+                    </div>
+                    <div className="border-l-2 border-slate-200 pl-8">
+                        <h1>
+                            <UsernameLink username={post.user.username} />
+                            &nbsp;
+                            {post.caption}
+                        </h1>
+                        <p>{post.likes}</p>
+                    </div>
                 </div>
-                <div className="border-l-2 border-slate-200 pl-8">
-                    <h1>
-                        <UsernameLink username={post.user.username} />
-                        &nbsp;
-                        {post.caption}
-                    </h1>
-                    <p>{post.likes}</p>
-                </div>
+                <CommentForm />
+                {post.comments?.map((comment) => (
+                    <CommentItem key={comment.id} comment={comment} />
+                ))}
             </div>
         </Basic>
     );
