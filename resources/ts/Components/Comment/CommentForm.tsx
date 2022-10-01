e3a92f7cd1eb4319cc0c575112093db049607e2d
Alan <realalansan@gmail.com>
2022-10-01 03:26:12 -0700

    Add business logic

--- resources/ts/Components/Comment/CommentForm.tsx
+++ resources/ts/Components/Comment/CommentForm.tsx
@@ -1,13 +1,38 @@
+import { Post } from "@/Types/models";
 import { useForm } from "@inertiajs/inertia-react";
+import React from "react";
 
-interface Props {}
+interface Props {
+    post: Post;
+}
 
-const CommentForm: React.FC<Props> = ({}: Props) => {
-    const { data } = useForm();
+const CommentForm: React.FC<Props> = ({ post }: Props) => {
+    const {
+        data,
+        setData,
+        post: postMethod,
+    } = useForm({
+        content: "",
+        commentable_type: "post",
+        commentable_id: post.id,
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.name === "content") {
+            setData(e.target.name, e.target.value);
+        }
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        postMethod("/comments");
+    };
 
     return (
         <form
             action="submit"
+            onSubmit={(e) => handleSubmit(e)}
             className="bg-slate-100 rounded px-4 pb-4 pt-2 shadow"
         >
             <h2 className="text-lg mb-2 mt-1">Comment</h2>
@@ -16,7 +41,8 @@ const CommentForm: React.FC<Props> = ({}: Props) => {
                     type="text"
                     id="comment"
                     name="comment"
-                    value={data.comment}
+                    value={data.content}
+                    onChange={(e) => handleChange(e)}
                     placeholder="Write your thoughts..."
                     className="border-slate-200 border-b-slate-300 rounded w-full shadow"
                 />
