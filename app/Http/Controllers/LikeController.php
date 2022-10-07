a346e17e09fe77cc140972c515ec5e5b0c403a65
Alan <realalansan@gmail.com>
2022-10-07 08:17:15 -0700

    Add store method

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreLikeRequest;
+use App\Models\Like;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LikeController extends Controller
 {
-    //
+    public function store(StoreLikeRequest $request)
+    {
+        $validated = $request->validated();
+
+        $like = Like::create(array_merge($validated, ['user_id' => Auth::user()->id]));
+
+        return response()->json($like);
+    }
 }
